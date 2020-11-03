bee500b22f4f7ad21896a1b8f9c518dc10320879
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-11-03 04:29:59 +0000

    ホームページのコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'home_pages#home'
+  get 'home_pages/help'
+  get 'home_pages/privacy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
