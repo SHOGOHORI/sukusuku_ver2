f594fb5c1efe21346e65b7e04408bd1f0e732178
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-11-03 04:30:47 +0000

    ホームページのコントローラー作成

---
+++ spec/requests/home_pages_request_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "HomePages", type: :request do
+
+  describe "GET /home" do
+    it "returns http success" do
+      get "/home_pages/home"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /help" do
+    it "returns http success" do
+      get "/home_pages/help"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /privacy" do
+    it "returns http success" do
+      get "/home_pages/privacy"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
