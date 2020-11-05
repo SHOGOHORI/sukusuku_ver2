24dc8a3c51607200e3664a64b50bc514ff95b231
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-11-05 11:09:43 +0900

    ユーザー登録コントローラー、ビュー作成 #2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'home_pages#home'
   get  '/help', to: 'home_pages#help'
   get  '/privacy', to: 'home_pages#privacy'
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+    sessions: 'users/sessions'
+  }
+  get  '/signup', to: 'devise/registrations#new'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
