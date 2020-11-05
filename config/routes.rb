a8bcb8b4dee9f7ae57193a6cf3fb50c0fb60b375
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-11-05 16:44:29 +0900

    バリデーション変更 #2

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   get  '/privacy', to: 'home_pages#privacy'
 
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
-    sessions: 'users/sessions'
+    registrations: 'devise/registrations',
+    sessions: 'devise/sessions'
   }
   get  '/signup', to: 'devise/registrations#new'
+  get  '/login', to: 'devise/sessions#new'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
