4df3775734bb9c18fcbb2e0a51e09718317d30c5
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-11-17 23:42:44 +0900

    プロフィール編集フォーム追加 #11

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
+  validates :name, length: { maximum: 10 }
   validates :introduction, length: { maximum: 200 }
   has_many :children, dependent: :destroy, inverse_of: :user
   accepts_nested_attributes_for :children, allow_destroy: true
