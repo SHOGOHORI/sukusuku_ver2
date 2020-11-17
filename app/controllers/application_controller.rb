63d9723b53d7e1b6db50cee18031ab9bc8696a8a
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-11-17 14:59:56 +0900

    JS追加 #13

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+  # def noname_user
+  #   if user_signed_in?
+  #     redirect
+  #   end
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
          :rememberable, :validatable
   validates :introduction, length: { maximum: 200 }
   has_many :children, dependent: :destroy, inverse_of: :user
-  accepts_nested_attributes_for :children
+  accepts_nested_attributes_for :children, allow_destroy: true
 end
