53e31dc9e23a76b4c28d33febda91d97efe6a3bd
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-11-22 18:39:01 +0900

    capistrano設定変更2 #5

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
-server '18.177.13.105', user: 'shogo', roles: %w{app db web}
-
-set :ssh_options, keys: '~/.ssh/myapp_key_rsa'
+server '18.177.13.105', user: 'shogo',
+                        roles: %w{app db web},
+                        ssh_options: {
+                        keys: [File.expand_path('~/.ssh/myapp_key_rsa)')]
+                        }
