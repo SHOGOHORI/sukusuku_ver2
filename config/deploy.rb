fe30b09a05ab3ed601e7d5f21bce2e11da4ac4e1
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-11-22 15:35:24 +0900

    Capistrano設定変更 #5

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,48 +1,35 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
+# config valid only for current version of Capistrano
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+lock '3.14.1'
 
+# Capistranoのログの表示に利用する
 set :application, 'sukusuku_ver2'
-set :repo_url, 'git@github.com:SHOGOHORI/sukusuku_ver2.git'
-set :branch, 'main'
 set :deploy_to, '/var/www/rails/sukusuku_ver2'
-set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :keep_releases, 5
-set :rbenv_ruby, '2.7.2'
-set :log_level, :debug
 
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:SHOGOHORI/sukusuku_ver2.git'
+
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.7.2'
+
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/○sukusuku_key.pem']
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
-  desc 'Restart application'
   task :restart do
     invoke 'unicorn:restart'
-  end
-
-  desc 'Create database'
-  task :db_create do
-    on roles(:db) do |host|
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:create'
-        end
-      end
-    end
-  end
-
-  desc 'Run seed'
-  task :seed do
-    on roles(:app) do
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:seed'
-        end
-      end
-    end
-  end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-    end
   end
 end
--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -6,6 +6,7 @@ $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
 $std_log = File.expand_path 'log/unicorn.log', $app_dir
 # set config
 worker_processes  $worker
+app_path = File.expand_path('../../../', __FILE__)
 working_directory $app_dir
 stderr_path $std_log
 stdout_path $std_log
