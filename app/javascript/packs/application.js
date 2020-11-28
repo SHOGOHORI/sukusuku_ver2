febd601d3bdd2e5576dbdcdd94bb4cb45c2505b7
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-11-28 16:23:04 +0900

    プロフィールフォームのJavaScript修正 #17

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 require("@nathanvda/cocoon")
-require ("packs/users")
+require ("packs/users.js.erb")
 import "bootstrap"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
