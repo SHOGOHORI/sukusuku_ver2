d109d7b2b3479d94d261c4b816080e26cfe46d79
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-11-30 18:00:17 +0900

    capistrano変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,8 +10,8 @@ CarrierWave.configure do |config|
     config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: "AKIARCPUWMFE6BPS7FFE",
-      aws_secret_access_key: "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",
+      aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+      aws_secret_access_key: ENV['AWS_SECRET_KEY'],
       region: 'ap-northeast-1',
       path_style: true
     }
