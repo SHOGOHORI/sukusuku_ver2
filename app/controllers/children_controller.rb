7522454b0b1bfb1499f513e8d78bd087858e3a5a
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-12-01 12:29:34 +0900

    子どもの情報編集フォーム変更 #17

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,13 +1,13 @@
 class ChildrenController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :destroy]
+  before_action :set_child, only: [:edit, :update, :destroy]
 
   def edit
-    @children = @user.children
+    @child = Child.find(params[:id])
   end
 
   def update
-    if @user.update(user_params)
-      redirect_to @user, notice: '子どもの情報を更新しました。'
+    if @child.update(child_params)
+      redirect_to user_path(current_user), notice: '子どもの情報を更新しました。'
     else
       flash.now[:alert] = '必須項目を入力願います。'
       render :edit
@@ -15,17 +15,16 @@ class ChildrenController < ApplicationController
   end
 
   def destroy
-    @children = @user.children
-    @user.destroy
-    redirect_to users_url, flash: { success: '子供の情報を削除しました' }
+    @child.destroy
+    redirect_to user_path(current_user), flash: { success: '子供の情報を削除しました' }
   end
 
-  def user_params
-    params.require(:user).permit(children_attributes: [:id, :nick_name, :birthday, :avatar, :comment, :child_number, :_destroy])
+  def child_params
+    params.require(:child).permit(:nick_name, :birthday, :avatar, :comment, :child_number)
   end
 
-  def set_user
-    @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user == @user
+  def set_child
+    @child = Child.find(params[:id])
+    redirect_to(root_url) unless current_user.id == @child.user_id
   end
 end
