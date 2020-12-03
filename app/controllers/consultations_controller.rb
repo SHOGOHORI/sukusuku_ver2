b257040012f5c55d9c8bcd468b90478566e7f16b
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-12-03 15:54:37 +0900

    相談コントローラー作成 #20

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,2 +1,17 @@
 class ConsultationsController < ApplicationController
+
+
+  def new
+    @consultation = Consultation.new
+  end
+
+  def create
+
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:content, :title)
+  end
 end
