a8eb3925137dc118a87021c852126efb0949eef6
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-12-03 15:59:16 +0900

    相談コントローラーnew,create,show記入 #20

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -6,12 +6,21 @@ class ConsultationsController < ApplicationController
   end
 
   def create
+    @consultation = current_user.consultations.build(consultation_params)
+    if @consultation.save
+      redirect_to consultation_url(@consultation), flash: { success: '投稿しました' }
+    else
+      render :new, flash: { danger: '投稿に失敗しました' }
+    end
+  end
 
+  def show
+    @consultation = Consultation.find(params[:id])
   end
 
   private
 
-  def question_params
-    params.require(:question).permit(:content, :title)
+  def consultation_params
+    params.require(:consultation).permit(:content, :title)
   end
 end
