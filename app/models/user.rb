5d6c6f375536c59c7900a20bcccc019d04b91156
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-12-03 17:44:24 +0900

    consultation_newビュー調整 #20

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
+  has_many :microposts, dependent: :destroy
+  has_many :children, dependent: :destroy, inverse_of: :user
   validates :name, length: { maximum: 10 }
   validates :introduction, length: { maximum: 200 }
-  has_many :children, dependent: :destroy, inverse_of: :user
   accepts_nested_attributes_for :children, reject_if: :all_blank, allow_destroy: true
   mount_uploader :avatar, AvatarUploader
-  has_many :microposts
 end
