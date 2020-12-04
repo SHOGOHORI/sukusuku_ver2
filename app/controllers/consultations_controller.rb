a8f79da46ecf9bb0cc7c3706697bb19c35c16b89
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-12-04 14:37:13 +0900

    投稿画面変更 #20

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -21,6 +21,6 @@ class ConsultationsController < ApplicationController
   private
 
   def consultation_params
-    params.require(:consultation).permit(:content, :title, :user_id, {images: []})
+    params.require(:consultation).permit(:content, :title, :user_id, { images: [] })
   end
 end
--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -1,6 +1,6 @@
 class Consultation < ApplicationRecord
   belongs_to :user
-  mount_uploader :image, ImageUploader
+  mount_uploaders :images, ImageUploader
   validates :title, presence: true
   validates :content, presence: true
   validates :user_id, presence: true
