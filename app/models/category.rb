5b33d27a6883bf9d0bee1a28783aecedea708118
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-12-05 14:53:16 +0900

    DB関連付け #21

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
+  has_one :consultation
   validates :category, length: { maximum: 10 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_05_051311) do
+ActiveRecord::Schema.define(version: 2020_12_05_054759) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "category"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_12_05_051311) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.json "image"
+    t.integer "category_id"
     t.index ["user_id", "created_at"], name: "index_consultations_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_consultations_on_user_id"
   end
