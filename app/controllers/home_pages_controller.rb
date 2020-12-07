1b4c690f3664b7441c4d6d44ae1176aca4ff7c3b
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-12-07 20:48:48 +0900

    相談一覧画面作成 #22

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -1,7 +1,7 @@
 class HomePagesController < ApplicationController
   def home
     @consultations = Consultation.all
-    @consultations = Kaminari.paginate_array(@consultations).page(params[:page])
+    @consultations = Kaminari.paginate_array(@consultations).page(params[:page]).per(5)
   end
 
   def help
