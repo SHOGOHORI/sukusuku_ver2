dd3d1204080d34a3d2780910661b1728f3f0af23
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-12-08 19:43:52 +0900

    コメントコントローラー作成 #23

---
+++ app/controllers/consultation_comment_controller.rb
@@ -0,0 +1,7 @@
+class ConsultationCommentController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
---
+++ spec/requests/consultation_comment_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "ConsultationComments", type: :request do
+
+  describe "GET /new" do
+    it "returns http success" do
+      get "/consultation_comment/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/consultation_comment/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
