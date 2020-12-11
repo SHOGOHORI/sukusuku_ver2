94591e6bbca13437f29616f900162936482545b6
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-12-11 23:16:18 +0900

    バリデーション編集 #25

--- app/models/consultation_comment_reply.rb
+++ app/models/consultation_comment_reply.rb
@@ -1,4 +1,8 @@
 class ConsultationCommentReply < ApplicationRecord
   belongs_to :user
   belongs_to :consultation_comment
+  mount_uploaders :image, ImageUploader
+  validates :user_id, presence: true
+  validates :consultation_comment_id, presence: true
+  validates :content, presence: true, length: { maximum: 500 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :consultations, dependent: :destroy
   has_many :children, dependent: :destroy
   has_many :consultation_comments, dependent: :destroy
+  has_many :consultation_comment_replies, dependent: :destroy
   validates :name, length: { maximum: 10 }
   validates :introduction, length: { maximum: 200 }
   accepts_nested_attributes_for :children, allow_destroy: true
