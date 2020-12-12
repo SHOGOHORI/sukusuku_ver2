572782cc884146d8afd2f6b0750631977947f5e0
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-12-12 15:26:06 +0900

    返信機能追加 #25

--- app/controllers/consultation_comment_reply_controller.rb
+++ app/controllers/consultation_comment_reply_controller.rb
@@ -1,2 +1,27 @@
 class ConsultationCommentReplyController < ApplicationController
+  before_action :set_comment, only: :destroy
+
+  def create
+    @reply = ConsultationComment.new(comment_params)
+    if @reply.save
+      redirect_to consultation_url(@reply.consultsation_comment.consultation), notice: '投稿しました。'
+    else
+      flash.now[:alert] = '投稿に失敗しました'
+      render 'consultations/show'
+    end
+  end
+
+  def destroy
+    @reply.destroy
+    redirect_to request.referrer, notice: '削除しました'
+  end
+
+  def comment_params
+    params.require(:consultation_comment_reply).permit(:content, :user_id, :consultation_comment_id, { image: [] })
+  end
+
+  def set_reply
+    @reply = ConsultationCommentReply.find(params[:id])
+    redirect_to(root_url) unless current_user.id == @reply.user_id
+  end
 end
