b1a0ddc77687fee6efe377ba1ac5da667a4a617e
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-12-14 22:47:56 +0900

    カテゴリー検索機能作成 #27

--- app/javascript/packs/consultations.js
+++ app/javascript/packs/consultations.js
@@ -16,15 +16,15 @@ document.addEventListener("turbolinks:load"
 $(function(){
   $('#q_pregnant_eq').change(function(){
     if ($(this).val() == 1) {
-      $('#q_child_age_gteq').prop('disabled', true);
-      $('#q_child_moon_age_gteq').prop('disabled', true);
-      $('#q_child_age_lteq').prop('disabled', true);
-      $('#q_child_moon_age_lteq').prop('disabled', true);
+      $('#q_child_age_eq').prop('disabled', true);
+      $('#q_child_moon_age_eq').prop('disabled', true);
+      // $('#q_child_age_lteq').prop('disabled', true);
+      // $('#q_child_moon_age_lteq').prop('disabled', true);
       } else {
-      $('#q_child_age_gteq').prop('disabled', false);
-      $('#q_child_moon_age_gteq').prop('disabled', false);
-      $('#q_child_age_lteq').prop('disabled', false);
-      $('#q_child_moon_age_lteq').prop('disabled', false);
+      $('#q_child_age_eq').prop('disabled', false);
+      $('#q_child_moon_age_eq').prop('disabled', false);
+      // $('#q_child_age_lteq').prop('disabled', false);
+      // $('#q_child_moon_age_lteq').prop('disabled', false);
     }
   })
 });
