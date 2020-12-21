833d21f7111d56ff4aa37eefbfe4bf3de3303595
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-12-21 19:39:55 +0900

    vote_relationshipsモデル作成 #43

--- app/controllers/vote_relationships_controller.rb
+++ app/controllers/vote_relationships_controller.rb
@@ -1,8 +1,22 @@
 class VoteRelationshipsController < ApplicationController
 
   def create
+    @vote_relationship = VoteRelationship.new(vote_relationship_params)
+    @vote = @vote_relationship.vote
+    if @vote_relationship.save
+      redirect_to @vote_relationship.vote, notice: '投票しました。'
+    else
+      flash.now[:alert] = '投票に失敗しました'
+      render 'votes/show'
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def vote_relationship_params
+    params.require(:vote_relationship).permit(:user_id, :vote_id)
   end
 end
