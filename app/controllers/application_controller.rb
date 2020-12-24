6c2fdc18f98ff0901a879cb379373f435cc13721
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-12-24 11:54:36 +0900

    検索機能拡張 #43

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,6 @@ class ApplicationController < ActionController::Base
   before_action :set_category
 
   def set_search
-    # params[:q] ||= HashWithIndifferentAccess.new
-    # params[:q][:child_age_moon_age_lteq] = params[:q][:age_lteq].to_i * 12 + params[:q][:moon_age_lteq].to_i
-    # params[:q][:child_age_moon_age_gteq] = params[:q][:age_gteq].to_i * 12 + params[:q][:moon_age_gteq].to_i
     @q = Consultation.ransack(params[:q])
     @q.sorts = 'updated_at desc' if @q.sorts.empty?
     @search_consultations = Kaminari.paginate_array(@q.result).page(params[:page]).per(5)
@@ -33,7 +30,11 @@ class ApplicationController < ActionController::Base
 
   def store_current_location
     return if current_user
-
     store_location_for(:user, request.url)
+  end
+
+  def search_option
+    params[:q][:child_age_moon_age_lteq] = params[:q][:age_lteq].to_i * 12 + params[:q][:moon_age_lteq].to_i
+    params[:q][:child_age_moon_age_gteq] = params[:q][:age_gteq].to_i * 12 + params[:q][:moon_age_gteq].to_i
   end
 end
