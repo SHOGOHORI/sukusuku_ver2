1cd6d5a187f6fc43d06705e3c393ecd6f3c873f9
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-12-26 13:46:59 +0900

    アンケートコメントコントローラー作成 #48

--- app/controllers/vote_comments_controller.rb
+++ app/controllers/vote_comments_controller.rb
@@ -1,4 +1,29 @@
 class VoteCommentsController < ApplicationController
+  before_action :set_comment, only: :destroy
 
+  def create
+    @comment = VoteComment.new(comment_params)
+    if @comment.save
+      redirect_to @comment.vote, notice: '投稿しました。'
+    else
+      flash.now[:alert] = '投稿に失敗しました'
+      render 'votes/show'
+    end
+  end
 
+  def destroy
+    @comment.destroy
+    redirect_to request.referrer, notice: '削除しました'
+  end
+
+  private
+
+  def comment_params
+    params.require(:vote_comment).permit(:content, :user_id, :vote_id)
+  end
+
+  def set_comment
+    @comment = VoteComment.find(params[:id])
+    redirect_to(root_url) unless current_user.id == @comment.user_id
+  end
 end
--- app/models/vote_comment.rb
+++ app/models/vote_comment.rb
@@ -1,4 +1,8 @@
 class VoteComment < ApplicationRecord
   belongs_to :user
   belongs_to :vote
+  validates :user_id, presence: true
+  validates :vote_id, presence: true
+  validates :content, presence: true, length: { maximum: 500 }
+  scope :recently, -> { order(created_at: :desc) }
 end
