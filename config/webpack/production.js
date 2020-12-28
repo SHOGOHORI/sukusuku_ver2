7f6614ec08e9956d9cf765e27fbb5a451eb73c63
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-12-28 18:23:29 +0900

    AWSデプロイ用

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,5 +2,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
-module.exports = performance: { hints: false }
+module.exports = { environment.toWebpackConfig(),
+  performance: { hints: false }
+}
