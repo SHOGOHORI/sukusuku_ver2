fd42c8195e4bfc12add986ab70631bf30bc6e3ee
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-12-31 18:49:02 +0900

    closed_at追加 #45

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -9,6 +9,7 @@ class VotesController < ApplicationController
   def create
     @vote = Vote.new(vote_params)
     @vote.child_age_moon_age = @vote.age.to_i * 12 + @vote.moon_age.to_i
+    @vote.closed_at = Date.today + @vote.days.to_i
     if @vote.save
       redirect_to @vote, notice: '投稿しました。'
     else
@@ -20,11 +21,13 @@ class VotesController < ApplicationController
   def show
     store_location
     @vote = Vote.find(params[:id])
+    @vote.vote_items.each
     impressionist(@vote, nil, unique: [:session_hash])
     # @form = Form::ItemCollection.new
     @vote_relationship = VoteRelationship.new
     @comment = VoteComment.new
     @comments = @vote.vote_comments.recently.page(params[:page]).per(5)
+    @data = [['2019-06-01', 100], ['2019-06-02', 200], ['2019-06-03', 150]]
   end
 
   def destroy
@@ -35,7 +38,7 @@ class VotesController < ApplicationController
   private
 
   def vote_params
-    params.require(:vote).permit(:content, :title, :user_id, :category_id, :age, :moon_age, :pregnant, { image: [] }, vote_items_attributes: [:item, :item_number])
+    params.require(:vote).permit(:content, :title, :user_id, :category_id, :age, :moon_age, :pregnant, { image: [] }, :days, vote_items_attributes: [:item, :item_number])
   end
 
   def store_location
