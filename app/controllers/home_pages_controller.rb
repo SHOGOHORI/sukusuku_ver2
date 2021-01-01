63ea83e6df42d8a14a7bb7fb7f8fb318af38f0b4
SHOGOHORI <shogo.hori.1988@gmail.com>
2021-01-01 17:04:57 +0900

    ページネーション変更 #45

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -2,8 +2,8 @@ class HomePagesController < ApplicationController
   skip_before_action :store_current_location
 
   def home
-    @consultations = Consultation.all.recently.page(params[:consultation_page]).per(5)
-    @votes = Vote.all.recently.page(params[:vote_page]).per(5)
+    @consultations = Consultation.all.recently.page(params[:page]).per(5)
+    @votes = Vote.all.recently.page(params[:page]).per(5)
     respond_to do |format|
       format.html
       format.js
