de0983c8d54ba4c377f817baa6ab2d276834aa06
SHOGOHORI <shogo.hori.1988@gmail.com>
2021-01-09 22:53:52 +0900

    ページネーション機能見直し #65

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,17 +27,17 @@ document.addEventListener("turbolinks:load"
   $(document).ready(function() {
     $('.tab-content>div').hide();
     $('.tab-content>div').first().slideDown();
-      $('.tab-buttons span').click(function(){
-        var thisclass=$(this).attr('class');
-        $('#lamp').removeClass().addClass('#lamp').addClass(thisclass);
-        $('.tab-content>div').each(function(){
-          if($(this).hasClass(thisclass)){
-            $(this).fadeIn(800);
-          }
-          else{
-            $(this).hide();
-          }
-        });
+    $('.tab-buttons span').click(function(){
+      var thisclass=$(this).attr('class');
+      $('#lamp').removeClass().addClass('#lamp').addClass(thisclass);
+      $('.tab-content>div').each(function(){
+        if($(this).hasClass(thisclass)){
+          $(this).fadeIn(800);
+        }
+        else{
+          $(this).hide();
+        }
       });
-  })
+    });
+  });
 })
