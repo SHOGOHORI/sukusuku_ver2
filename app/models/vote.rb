73cf66b7dca016a7fb1c1d7d8dbd39c5f6472283
SHOGOHORI <shogo.hori.1988@gmail.com>
2021-01-10 12:17:07 +0900

    アンケート詳細画面カスタマイズ #65

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -7,7 +7,7 @@ class Vote < ApplicationRecord
   has_many :vote_comments, dependent: :destroy
   mount_uploaders :image, ImageUploader
   validates :title, presence: true, length: { maximum: 30 }
-  validates :content, presence: true, length: { maximum: 1000 }
+  validates :content, presence: true, length: { maximum: 500 }
   validates :user_id, presence: true
   validates :category_id, presence: true
   validates :closed_at, presence: true
