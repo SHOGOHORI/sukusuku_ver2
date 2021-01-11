9c619ebd6ea32096329f41a0584381232186d587
SHOGOHORI <shogo.hori.1988@gmail.com>
2021-01-11 16:05:15 +0900

    Imagesテーブル削除 #70

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_043625) do
+ActiveRecord::Schema.define(version: 2021_01_11_070335) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "category"
@@ -67,12 +67,6 @@ ActiveRecord::Schema.define(version: 2021_01_05_043625) do
     t.integer "child_age_moon_age"
     t.index ["user_id", "created_at"], name: "index_consultations_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_consultations_on_user_id"
-  end
-
-  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "image"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "impressions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
