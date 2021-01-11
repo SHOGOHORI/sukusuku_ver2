e97dc58544c2a907837ac75bbfd243012d73d3d2
SHOGOHORI <shogo.hori.1988@gmail.com>
2021-01-11 13:26:21 +0900

    マイページ変更 #70

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :vote_relationships
   has_many :vote_items, through: :vote_relationships
   validates :name, length: { maximum: 10 }
-  validates :introduction, length: { maximum: 200 }
+  validates :introduction, length: { maximum: 100 }
   accepts_nested_attributes_for :children, allow_destroy: true
   mount_uploader :avatar, AvatarUploader
 end
