12cf8d143b85c72f1ac1262a31a26a325c9e882c
SHOGOHORI <shogo.hori.1988@gmail.com>
2021-01-13 16:41:40 +0900

    consultation_stok機能作成# 50

--- app/controllers/consultation_stocks_controller.rb
+++ app/controllers/consultation_stocks_controller.rb
@@ -1,13 +1,21 @@
 class ConsultationStocksController < ApplicationController
 
   def create
+    @consultation = Consultation.find(params[:consultation_id])
     @stock = current_user.consultation_stocks.create(consultation_id: params[:consultation_id])
-    redirect_back(fallback_location: root_path)
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def destroy
+    @consultation = Consultation.find(params[:consultation_id])
     @stock= ConsultationStock.find_by(consultation_id: params[:consultation_id], user_id: current_user.id)
     @stock.destroy
-    redirect_back(fallback_location: root_path)
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
