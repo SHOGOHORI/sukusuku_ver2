d7a6c5cbbcbb705868281447fb2f8b4515391fb2
SHOGOHORI <shogo.hori.1988@gmail.com>
2021-01-14 22:53:55 +0900

    アンケート項目テスト追加 #75

--- spec/models/vote_item_spec.rb
+++ spec/models/vote_item_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe VoteItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'アンケート項目' do
+    let!(:user) { create(:user) }
+    let!(:category) { create(:category) }
+    let!(:vote) do
+      create(:vote, category_id: category.id,
+                            user_id: user.id)
+    end
+    let(:vote_item) do
+      build(:vote_item,  item: item,
+                         item_number: item_number,
+                         vote_id: vote_id)
+    end
+    subject { vote_item }
+    context '有効なアンケート項目' do
+      let(:item) { '項目１' }
+      let(:item_number) { 1 }
+      let(:vote_id) { vote.id }
+      it { is_expected.to be_valid }
+    end
+  end
 end
