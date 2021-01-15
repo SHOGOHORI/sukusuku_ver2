3cdee0eeefdeb228ea47a47bc956e1ef2d47abe4
SHOGOHORI <shogo.hori.1988@gmail.com>
2021-01-15 11:34:51 +0900

    モーダルフォーム修正 #75

--- app/models/child.rb
+++ app/models/child.rb
@@ -4,6 +4,12 @@ class Child < ApplicationRecord
   validates :child_number, presence: true
   validates :nick_name, presence: true, length: { maximum: 10 }
   validates :birthday, presence: true
+  validate :child_birthday
   validates :comment, length: { maximum: 200 }
   mount_uploader :avatar, AvatarUploader
+
+  def child_birthday
+    errors.add(:birthday, 'は3歳までのお子さん及び出産予定日を入力してください。') if
+    birthday < Date.today.next_year || birthday > Date.today.ago(3.years)
+  end
 end
