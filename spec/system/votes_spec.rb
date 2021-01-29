9fe9e189b46d806f03333e4b85185365e41aa2b0
SHOGOHORI <shogo.hori.1988@gmail.com>
2021-01-29 10:48:38 +0900

    ヘッダー、フッター色変更 #92

--- spec/system/votes_spec.rb
+++ spec/system/votes_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Votes", type: :system do
-  before do
-    driven_by(:rack_test)
+  scenario '有効なアンケート投稿' do
+    user = create(:user)
+    visit user_session_path
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード', with: user.password
+    check 'パスワードを記憶'
+    click_button 'ログイン'
+    expect(current_path).to eq user_path(user)
+    visit current_path
+    click_link 'アンケート'
+    expect(current_path).to eq new_vote_path
+    fill_in 'タイトル', with: 'テスト'
+    fill_in 'タイトル', with: 'テスト'
   end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
 end
